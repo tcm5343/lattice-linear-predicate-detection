a6f34262d6697a7dd1983edabc80574213249642
tcm5343 <gunstuff1@tutanota.com>
2023-10-21 12:58:38 -0500

    init starting values for ppx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ package main.java;
 
 class Main {
     public static void main(String[] args) {
+        // starting with PPx
+        int[] A = new int[]{3, 2, 4, 2, 1, 3, 5, 2};  // values taken from Chapter 4.2
         System.out.println("hello");
     }
 }
