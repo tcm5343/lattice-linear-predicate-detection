6b3c0302472369a24849eb708e408f8da551d35b
tcm5343 <gunstuff1@tutanota.com>
2023-10-21 13:02:11 -0500

    remov eprint lint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ class Main {
     public static void main(String[] args) {
         // starting with PPx
         int[] A = new int[]{3, 2, 4, 2, 1, 3, 5, 2};  // values taken from Chapter 4.2
-        System.out.println("hello");
+
     }
 }
